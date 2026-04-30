d83aad5ea75939e272b5c763f732980071290ac4
XieJiaFeng <13324056@qq.com>
2026-04-30 15:04:29 +0800

    fix: 修复登录页面翻译
    添加login翻译到zh-CN.json:
    - 登录标题、副标题
    - 用户名/密码占位符
    - 记住我复选框
    - 登录按钮
    - 注册/忘记密码链接
    
    Co-authored-by: xie-jia-feng <xie@company.com>

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6d565866'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ea35ec24h4o"
+    "revision": "0.ffo2iimvj9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
